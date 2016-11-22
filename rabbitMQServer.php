789c1522ee1fc977a17fcbac32651ba62ddb26e9
ses10 <sesma@live.com>
2016-11-21 22:30:07 -0500

    add deploybundle and deployAlert

--- rabbitMQServer.php
+++ rabbitMQServer.php
@@ -40,6 +40,48 @@ function updateBundleVer($request)
 		
 }
 
+function deployBundle($request)
+{
+	$json = file_get_contents('./hosts.json');
+	$arr = json_decode($json, true);
+	
+	//get ip of target 
+	$target = ($arr[$request['bundle']][$request['machine']] );
+	$ip = $target['ip'];
+	
+	echo "\n" . "Doing scp" . "\n";
+	
+        $dbHelper = new DatabaseHelper();
+        $dbHelper->connect();
+        $ver = $dbHelper->getNextVersion($request['bundle']);
+	$ver -= 1;	
+	$bundle = $request['bundle'] . $ver; 
+
+	//scp 
+	$command= 'scp /home/ses/bundles/' . $bundle . ".tgz " . $target['user'] . "@" . $ip . ":/home/" . $target['user']. "/temp";
+	shell_exec($command);
+	
+	//change ini according to targetmachine
+	shell_exec('python3 /home/ses/deploymentProcessor/changeIni.py ' . $ip);
+	
+        //message target rabbitmq about recent deployment
+        $client = new rabbitMQClient("targetRabbitMQ.ini","testServer");
+	$request = array();
+	$request['type'] = 'deployAlert';
+	$request['version'] = $ver;
+	$request['message'] = 'hey gaybo';
+	$client->publish($request);
+		
+
+	echo 'recived deploybundle';
+	return $ip;
+}
+
+function deployAlert($request)
+{
+echo "\n Installing " . $request['version']. " \n";
+}
+
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -53,7 +95,11 @@ function requestProcessor($request)
     case "bundleRequest":
 	return nextBundleVer($request);
     case "updateBundleVer":
-	return updateBundleVer($request);	
+	return updateBundleVer($request);
+    case "deployBundle":
+	return deployBundle($request);
+    case "deployAlert":
+	deployAlert($request);	
   }    
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
